98b95ca779f793f11ade186b85e33b276998bc3c
hanks-su <42451381+hanks-su@users.noreply.github.com>
2020-02-24 19:50:16 -0800

    Update search.py

--- Week6Feb24/search.py
+++ Week6Feb24/search.py
@@ -1,29 +1,19 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
+import boto3
+from boto3.dynamodb.conditions import Key
+import json
+from decimal import Decimal
 import sys
 
-#  Read the tfidf dictionary  in from disk -- we want to get rid of this
-def ingest():
-    tfidfs = {}
-    with open("tfidf.txt") as f:
-        for line in f:
-            doc_id, term, tfidf = line.strip().split('\x01')
-            tdict = tfidfs.get(term, {})
-            tdict[doc_id] = float(tfidf)
-            tfidfs[term] = tdict
-    return tfidfs
-
-# Second argument is the dictionary:  term -> {{docid -> value}}
-# We need to get a retrieve from dynamodb to look like this same dictionary.
-# Cheat code below
- 
-def compute(terms, tfidfs):
-    docs = {}
-    for term in terms:
-        docs = mergeSum(docs, tfidfs.get(term, {}))
-    tuples = [(k, v) for k, v in docs.items()]
-    tuples = filter(lambda t: t[1] > 0, tuples)
-    return sorted(tuples, key=lambda x: x[1], reverse=True)
+# Return a dictionary {docid: tfidf} for all docs that mention this term
+def term_tfidf(term, table):
+    res = {}
+    response = table.query(KeyConditionExpression=Key('term').eq(term))
+    for item in response['Items']:
+        doc_id = item['docid']
+        tfidf = item['value']
+        res[doc_id] = float(tfidf)
+    return res
 
 def mergeSum(dict1, dict2):
     dict3 = {}
@@ -37,38 +27,47 @@ def mergeSum(dict1, dict2):
             dict3[key] = dict2[key]
     return dict3
 
-def termify(line):
+def compute(terms):
+    client = boto3.resource('dynamodb')
+    table = client.Table('tfidf')
+    docs = {}
+    for term in terms:
+        docs = mergeSum(docs, term_tfidf(term, table))
+    tuples = [(k, v) for k, v in docs.items()]
+    tuples = filter(lambda t: t[1] > 0, tuples)
+    return sorted(tuples, key=lambda x: x[1], reverse=True)
+
+def termify(l):
+    words = l.strip().split()
     terms = []
-    for word in line.strip().split():
+    for word in words:
         lowered = word.lower()
-        term = filter(lambda c: 97 <= ord(c) <= 122, lowered)
+        term = ''.join(list(filter(lambda c: 97 <= ord(c) <= 122, lowered)))
         if len(term) > 0:
             terms.append(term)
     return terms
 
+def dowords(words):
+    results = compute(words)
+    retval = []
+    for i in range(0, min(5, len(results))):
+        r = results[i]
+        retval.append({'doc_id': r[0], 'score': int(r[1]*10000000)})
+    return retval
+
 def dosearch():
-    tfidfs = ingest()
     while (True):
         sys.stdout.write("Query: ")
-        iline = raw_input()
-        results = compute(termify(iline), tfidfs)
+        iline = input()
+        results = dowords(termify(iline))
         for i in range(0, min(5, len(results))):
             r = results[i]
-            print("{0}. {1} -- {2}".format(i+1, r[0], int(r[1]*10000000)))
+            #print("{0}. {1} -- {2}".format(i+1, r[0], int(r[1]*10000000)))
+            print(r)
 
-    
+
 if __name__== "__main__":
   dosearch()
 
-'''
- client = boto3.resource('dynamodb')
-    table = client.Table('tfidf')
-def term_tfidf(term, table):
-    res = {}
-    response = table.query(KeyConditionExpression=Key('term').eq(term))
-    for item in response['Items']:
-        doc_id = item['doc_id']
-        tfidf = item['value']
-        res[doc_id] = float(tfidf)
-    return res
-'''
+
+
